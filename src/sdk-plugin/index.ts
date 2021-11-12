e2de1f55e2ae986d15f55b2d106b97003576b214
Amitkanswal <amitkanswal7@gmail.com>
2021-11-12 12:18:06 +0530

    feat:updated env variable [ECO-270]

--- src/sdk-plugin/index.ts
+++ src/sdk-plugin/index.ts
@@ -1,14 +1,14 @@
 import contentstack from 'contentstack';
 
 const Stack = contentstack.Stack(
-  process.env.CONTENTSTACK_API_KEY,
-  process.env.CONTENTSTACK_DELIVERY_KEY,
-  process.env.CONTENTSTACK_ENVIRONMENT,
-  process.env.CONTENTSTACK_REGION ? process.env.CONTENTSTACK_REGION : "us"
+  process.env.API_KEY,
+  process.env.DELIVERY_KEY,
+  process.env.ENVIRONMENT,
+  process.env.REGION ? process.env.REGION : "us"
 );
 
-// if (process.env.CONTENTSTACK_CUSTOM_HOST) {
-//   Stack.setHost(process.env.CONTENTSTACK_CUSTOM_HOST);
+// if (process.env.CUSTOM_HOST) {
+//   Stack.setHost(process.env.CUSTOM_HOST);
 // }
 
 export default {
