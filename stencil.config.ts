5d203653abf1ba85716c3bb5dc1801a936d12245
MaitriGurey1 <maitri.gurey@contentstack.com>
2023-05-05 15:07:35 +0530

    Added branch & region support

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,16 +4,20 @@ import fs from 'fs';
 
 export const config: Config = {
   plugins: fs.existsSync('./.env') ? [dotenvPlugin()] : [],
-  env: !fs.existsSync('./.env') ? {
-    CONTENTSTACK_API_KEY: process.env.CONTENTSTACK_API_KEY,
-    CONTENTSTACK_DELIVERY_TOKEN: process.env.CONTENTSTACK_DELIVERY_TOKEN,
-    CONTENTSTACK_ENVIRONMENT: process.env.CONTENTSTACK_ENVIRONMENT,
-    CONTENTSTACK_MANAGEMENT_TOKEN: process.env.CONTENTSTACK_MANAGEMENT_TOKEN,
-    CONTENTSTACK_API_HOST: process.env.CONTENTSTACK_API_HOST,
-    CONTENTSTACK_APP_HOST: process.env.CONTENTSTACK_APP_HOST,
-    CONTENTSTACK_LIVE_PREVIEW: process.env.CONTENTSTACK_LIVE_PREVIEW,
-    CONTENTSTACK_LIVE_EDIT_TAGS: process.env.CONTENTSTACK_LIVE_EDIT_TAGS
-  } : {},
+  env: !fs.existsSync('./.env')
+    ? {
+        CONTENTSTACK_API_KEY: process.env.CONTENTSTACK_API_KEY,
+        CONTENTSTACK_DELIVERY_TOKEN: process.env.CONTENTSTACK_DELIVERY_TOKEN,
+        CONTENTSTACK_ENVIRONMENT: process.env.CONTENTSTACK_ENVIRONMENT,
+        CONTENTSTACK_BRANCH: process.env.CONTENTSTACK_BRANCH,
+        CONTENTSTACK_REGION: process.env.CONTENTSTACK_REGION,
+        CONTENTSTACK_MANAGEMENT_TOKEN: process.env.CONTENTSTACK_MANAGEMENT_TOKEN,
+        CONTENTSTACK_API_HOST: process.env.CONTENTSTACK_API_HOST,
+        CONTENTSTACK_APP_HOST: process.env.CONTENTSTACK_APP_HOST,
+        CONTENTSTACK_LIVE_PREVIEW: process.env.CONTENTSTACK_LIVE_PREVIEW,
+        CONTENTSTACK_LIVE_EDIT_TAGS: process.env.CONTENTSTACK_LIVE_EDIT_TAGS,
+      }
+    : {},
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
@@ -22,14 +26,10 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: {
-        globPatterns: [
-          '**/*.{ts,css,json,html,ico,png}'
-        ]
+        globPatterns: ['**/*.{ts,css,json,html,ico,png}'],
       },
       baseUrl: 'https://myapp.local/',
-      copy: [
-        { src: 'robots.txt' }
-      ]
+      copy: [{ src: 'robots.txt' }],
     },
   ],
 };
